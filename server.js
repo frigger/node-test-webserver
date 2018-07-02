ef12a7e33da8017c0dc66a349e75efc650c976b7
Arm <saharat.jindawuthiphan@gmail.com>
2018-07-02 19:52:33 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log(err.message);
+        }
+    });
+    next();
+})
+
+app.use((req, res, next) => {
+    res.render('maintenence.hbs');
+});
+
+hbs.registerHelper('getCurrentYear', () =>{
+    return new Date().getFullYear();
+});
+hbs.registerHelper('toUpper', (text) => {
+    return String(text).toUpperCase();
+})
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
+})
+
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port);
+});
